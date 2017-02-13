01f807c002325b01752ed138ab0bc5f958355d74
John-Evans Wagenaar <john-evans.wagenaar@preloaded.com>
2017-02-13 11:55:22 +0000

    Add an attribute to legacy scripting.

--- spinner.js
+++ spinner.js
@@ -1,17 +1,19 @@
+pc.script.attribute('speed', 'number', 80);
+
 pc.script.create("spinner", function (app) {
-    var ScriptObject = function (entity) {
+    var Spinner = function (entity) {
         this.entity = entity;
     };
 
-    ScriptObject.prototype = {
+    Spinner.prototype = {
         initialize: function () {
 
         },
 
         update: function (dt) {
-        	this.entity.rotate(0, 1*dt, 0);
+        	this.entity.rotate(0, this.speed * dt, 0);
         }
     };
 
-    return ScriptObject;
+    return Spinner;
 });
