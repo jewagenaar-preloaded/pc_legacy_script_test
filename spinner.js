166ef249c5ca84cf2800e93bde32b7172f235aeb
John-Evans Wagenaar <john-evans.wagenaar@preloaded.com>
2017-02-13 11:09:17 +0000

    Initial commit

---
+++ spinner.js
@@ -0,0 +1,11 @@
+var Spinner = pc.createScript('spinner');
+
+// initialize code called once per entity
+Spinner.prototype.initialize = function() {
+    
+};
+
+// update code called every frame
+Spinner.prototype.update = function(dt) {
+    
+};
